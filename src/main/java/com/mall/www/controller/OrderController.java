15c25f81b6a2f065365ee5f3f5db6afd16fcaba7
ybq <hello@ybq.com>
2020-11-10 23:06:37 +0800

    添加 OrderController

---
+++ src/main/java/com/mall/www/controller/OrderController.java
@@ -0,0 +1,48 @@
+package com.mall.www.controller;
+
+import com.mall.www.common.ResponseEntity;
+import com.mall.www.common.dto.OrderDto;
+import com.mall.www.common.vo.OrderPrepareVo;
+import com.mall.www.service.OrderService;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+/**
+ * 订单
+ * <p>
+ * 订单准备     结算页面
+ * 需要用户id (查地址，优惠卷)
+ * 需要商品：id、商品数量、单价、数量,名称（不用购物车id，是考虑用户不加入购物车直接购买）
+ */
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+    @Resource
+    OrderService orderService;
+
+    /**
+     * 订单准备     结算页面
+     * 需要用户id (查地址，优惠卷)
+     * 需要商品：id、商品数量、单价、数量,名称
+     * （不用购物车id，是考虑用户不加入购物车直接购买）
+     *
+     * @param orderDto 用户id，商品id集合,及对象的商品数量
+     * @return
+     */
+    @PostMapping("/prepare")
+    public ResponseEntity<OrderPrepareVo> prepare(@RequestBody OrderDto orderDto) {
+        return ResponseEntity.success(orderService.prepare(orderDto));
+    }
+
+    /***
+     * 创建订单
+     * @param orderDto
+     * @return
+     */
+    @PostMapping("/create")
+    public ResponseEntity create(@RequestBody OrderDto orderDto) {
+        return ResponseEntity.success(orderService.create(orderDto));
+    }
+}
