0043517b2b7d336cf8f336bf006dbdb508f6a82a
ybq <hello@ybq.com>
2020-11-11 15:05:04 +0800

    修改 StockOutMapper

--- src/main/java/com/mall/www/mapper/StockOutMapper.java
+++ src/main/java/com/mall/www/mapper/StockOutMapper.java
@@ -1,4 +1,14 @@
 package com.mall.www.mapper;
 
+import com.mall.www.entity.Stock;
+import com.mall.www.entity.StockOut;
+import org.apache.ibatis.annotations.Param;
+
 public interface StockOutMapper {
+    /**
+     * 添加出库记录
+     * @param stockOut
+     * @return
+     */
+    int insert(@Param("stockOut") StockOut stockOut);
 }
