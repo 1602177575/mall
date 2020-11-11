4de7814f5b09cf5fc2bcd4901199ae3c648ace0b
ybq <hello@ybq.com>
2020-11-11 15:03:40 +0800

    修改 StockMapper

--- src/main/java/com/mall/www/mapper/StockMapper.java
+++ src/main/java/com/mall/www/mapper/StockMapper.java
@@ -8,9 +8,26 @@ import java.util.List;
 public interface StockMapper {
     /**
      * 根据商品id集合查询库存列表
-     * @param productIds    商品id集合
+     *
+     * @param productIds 商品id集合
      * @return
      */
     List<Stock> selectByProductIds(@Param("productIds") List<Long> productIds);
+
+    /**
+     * 根据商品id查询库存
+     *
+     * @param productId
+     * @return
+     */
+    Stock selectByProductId(@Param("productId") Long productId);
+
+    /**
+     * 根据商品id修改总库存
+     * @param productId 商品id
+     * @param total 总库存
+     * @return
+     */
+    int updateTotalByProductId(@Param("productId") Long productId, @Param("total") Long total);
 
 }
